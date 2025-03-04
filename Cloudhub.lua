9cb02469499708aa81a05082873471aad98d2bf9
Cloud <leboi3595@gmail.com>
2025-03-04 09:45:34 -0600

    Update Cloudhub.lua

--- Cloudhub.lua
+++ Cloudhub.lua
@@ -8,7 +8,7 @@ local serv = win:Server("Preview", "")
 local btns = serv:Channel("Buttons")
 
 btns:Button(
-    "Kill all",
+    "Mobile Keyboard",
     function() print(loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))())
         DiscordLib:Notification("Notification", "Killed everyone!", "Okay!")
     end
@@ -17,8 +17,19 @@ btns:Button(
 btns:Seperator()
 
 btns:Button(
-    "Get max level",
+    "FLY GUI",
     function()
+    print(loadstring(game:HttpGet('https://pastebin.com/raw/YSL3xKYU'))())
+        DiscordLib:Notification("Notification", "Max level!", "Okay!")
+    end
+)
+
+btns:Seperator()
+
+btns:Button(
+    "Touch Fling Script",
+    function()
+    print(loadstring(game:HttpGet("https://raw.githubusercontent.com/0Ben1/fe./main/Fling%20GUI"))())
         DiscordLib:Notification("Notification", "Max level!", "Okay!")
     end
 )
