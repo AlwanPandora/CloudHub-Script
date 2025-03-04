a17a90cc658feec50a637e79378b6e9cbbcbc71c
Cloud <leboi3595@gmail.com>
2025-03-04 09:26:57 -0600

    Update Cloudhub.lua

--- Cloudhub.lua
+++ Cloudhub.lua
@@ -109,7 +109,7 @@ bnds:Bind(
     "Kill bind",
     Enum.KeyCode.RightShift,
     function()
-        print("loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()")
+        print(loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt, true))()")
     end
 )
 
