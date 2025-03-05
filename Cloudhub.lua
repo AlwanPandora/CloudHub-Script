231193533af1ef98b80a096b3e4064e72f6b0259
Cloud <leboi3595@gmail.com>
2025-03-04 20:47:58 -0600

    Update Cloudhub.lua

--- Cloudhub.lua
+++ Cloudhub.lua
@@ -1,6 +1,6 @@
 local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord"))()
 
-local win = DiscordLib:Window("Discord Library")
+local win = DiscordLib:Window("CloudHub Script")
 
 local serv = win:Server("CloudHub Script", "")
 
